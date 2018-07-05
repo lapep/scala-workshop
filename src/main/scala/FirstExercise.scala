15d0e57e1f49e3acc5b814b8e389e0b10ccb0b9d
plape <lape.povilas@gmail.com>
2018-07-05 12:06:21 +0100

    Adding missing return type

--- src/main/scala/FirstExercise.scala
+++ src/main/scala/FirstExercise.scala
@@ -24,7 +24,7 @@ object FirstExercise {
   }
 
   //Hint1: need to get all the strings to one List, Hint2: make sure char case is ignored, Hint3: lookup other higher order functions available on Lists
-  def findMostFrequentLetter(listOfEmployees: List[Employee]) = {
+  def findMostFrequentLetter(listOfEmployees: List[Employee]): (Char, Int) = {
     val allStrings = listOfEmployees
       .foldLeft(List.empty[String]){(acc, empl) =>
         empl.name :: empl.benefits ++ acc
